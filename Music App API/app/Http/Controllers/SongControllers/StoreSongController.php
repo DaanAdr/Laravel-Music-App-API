1ad295c9abf646672f87d62c34f809ca17164183
DaanAdr <parsa1815@outlook.com>
2025-02-06 13:28:39 +0100

    Divided the StoreSongController into seperate functions

--- Music App API/app/Http/Controllers/SongControllers/StoreSongController.php
+++ Music App API/app/Http/Controllers/SongControllers/StoreSongController.php
@@ -13,13 +13,18 @@ class StoreSongController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $request->validate([
-            'name' => 'required|regex:/^[\p{L} ]+$/u', // This regex allows letters and spaces
-            'artist_id' => 'required|exists:artists,id', // Assuming you have an artists table
-            'genre_id' => 'required|exists:genres,id',
-            'feature_id' => 'nullable|exists:features,id'
-        ]);
+        $this->validateRequest($request);
     
         return Song::create($request->all());
+    }
+
+    private function validateRequest(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|regex:/^[\p{L} ]+$/u', // This regex allows letters and spaces
+            'artist_id' => 'required|exists:artists,id|regex:/^[0-9]+$/', // This regex allows only numbers
+            'genre_id' => ['required', 'exists:genres,id', 'regex:/^[0-9]+$/'],
+            'feature_id' => 'nullable|exists:features,id'
+        ]);
     }
 }
